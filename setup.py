632150da1a095119e6d411180de65b65f6cc9708
sasakingoo <y.sasaki.1305@gmail.com>
2017-09-08 23:20:51 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='seleniumbase',
+    version='0.0.1',
+    description='selenium and unittest base modules.',
+    author='Yuya Sasaki',
+    author_email=' y.sasaki.1305@gmail.com',
+    url='https://github.com/sasakingoo/seleniumbase',
+    install_requires=['selenium'],
+    license='MIT',
+    packages=['seleniumbase']
+)
